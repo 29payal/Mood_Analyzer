b31f3e5dc85071149c6079e10f44e3c914da92ff
Payal Sharma <https://github.com/29payal>
2022-09-21 19:16:35 +0530

    [Payal] UC1-AnalyzeHappyOrSad

--- UnitTest1/UnitTest1.cs
+++ UnitTest1/UnitTest1.cs
@@ -11,18 +11,24 @@ namespace UnitTest1
         
         public void GivenMsgReturnHappyOrSad()
         {
-
             //UC1- given the return message the mood is happy or sad
             AnalyzeMood analyzeMood = new AnalyzeMood();
-
             string message = "I am in happy mood";
             string expected = "happy";
-
             var actual = analyzeMood.Mood(message);
             Assert.AreEqual(expected, actual);
-
             Console.WriteLine(actual);
-
+        }
+        [TestMethod]
+        public void GivenMsgReturnSad()
+        {
+            //TC1.1- given the return message sad
+            AnalyzeMood analysisMood = new AnalyzeMood();
+            string message = "I am in Sad Mood";
+            string expected = "sad";
+            var actual = analysisMood.Mood(message);
+            Assert.AreEqual(expected,actual);
+            Console.WriteLine(actual);
         }
     }
 }
